f31f407491c9f3bf8e5d80399d161b7bc5dae2d7
kartik9555 <kartik9555@gmail.com>
2026-08-06 21:12:08 +0200

    From Text to Speech - Generating Voices with TextToSpeechModel

--- section10/src/main/java/com/learning/section10/controller/AudioController.java
+++ section10/src/main/java/com/learning/section10/controller/AudioController.java
@@ -4,6 +4,11 @@ import com.openai.models.audio.AudioResponseFormat;
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
 import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
 import org.springframework.ai.audio.transcription.TranscriptionModel;
+import org.springframework.ai.audio.tts.TextToSpeechModel;
+import org.springframework.ai.audio.tts.TextToSpeechOptions;
+import org.springframework.ai.audio.tts.TextToSpeechPrompt;
+import org.springframework.ai.audio.tts.TextToSpeechResponse;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -11,14 +16,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @RestController
 @RequestMapping("/api")
 public class AudioController {
 
     private final TranscriptionModel transcriptionModel;
+    private final TextToSpeechModel textToSpeechModel;
 
-    public AudioController(TranscriptionModel transcriptionModel) {
+    public AudioController(TranscriptionModel transcriptionModel, TextToSpeechModel textToSpeechModel) {
         this.transcriptionModel = transcriptionModel;
+        this.textToSpeechModel = textToSpeechModel;
     }
 
     @GetMapping("/transcribe")
@@ -40,5 +52,30 @@ public class AudioController {
         );
         AudioTranscriptionResponse response = transcriptionModel.call(prompt);
         return response.getResult().toString();
+    }
+
+    @GetMapping("/speech")
+    String speech(String message) throws IOException {
+        byte[] audioBytes = textToSpeechModel.call(message);
+        Path path = Paths.get("output.mp3");
+        Files.write(path, audioBytes);
+        return "MP3 saved successfully to: " + path.toAbsolutePath() ;
+    }
+
+    @GetMapping("/speech-options")
+    String speechWithOptions(String message) throws IOException {
+        TextToSpeechResponse call = textToSpeechModel.call(
+                new TextToSpeechPrompt(
+                        message,
+                        TextToSpeechOptions.builder()
+                                .voice(OpenAiAudioSpeechOptions.Voice.NOVA.getValue())
+                                .speed(2.0)
+                                .format(OpenAiAudioSpeechOptions.AudioResponseFormat.MP3.getValue())
+                                .build()
+                )
+        );
+        Path path = Paths.get("speech-options.mp3");
+        Files.write(path, call.getResult().getOutput());
+        return "MP3 saved successfully to: " + path.toAbsolutePath() ;
     }
 }
